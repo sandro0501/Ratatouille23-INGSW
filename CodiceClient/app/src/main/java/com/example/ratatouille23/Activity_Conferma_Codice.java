12131938652bd152fd13cbaa49d9e95f87655698
FerdiDale <fer.dalessandro@studenti.unina.it>
2023-01-02 12:13:58 +0100

    Aggiunta activity Conferma Codice

---
+++ CodiceClient/app/src/main/java/com/example/ratatouille23/Activity_Conferma_Codice.java
@@ -0,0 +1,13 @@
+package com.example.ratatouille23;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Conferma_Codice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conferma_codice);
+    }
+}
