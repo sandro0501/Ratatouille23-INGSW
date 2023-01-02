a1a6956c47f73eba75d999aa9f753e5fc6f5468d
KamiDaga <95750384+KamiDaga@users.noreply.github.com>
2023-01-02 11:14:09 +0100

    Add files via upload

--- Codice/app/src/main/java/com/example/ratatouille23/MainActivity.java
+++ Codice/app/src/main/java/com/example/ratatouille23/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ratatouille23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toggle(View c){
+        TextView t = (TextView) c;
+        t.setText("Cambiato");
     }
 }
