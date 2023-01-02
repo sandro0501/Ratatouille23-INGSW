7cd6f2a1b4ff4660c295e020e2da61c92de073b8
FerdiDale <fer.dalessandro@studenti.unina.it>
2023-01-02 18:28:28 +0100

    Aggiunta dell'interfaccia di conferma codice per il recupero password, incompleta

--- CodiceClient/app/src/main/java/com/example/ratatouille23/Activity_Conferma_Codice.java
+++ CodiceClient/app/src/main/java/com/example/ratatouille23/Activity_Conferma_Codice.java
@@ -2,12 +2,19 @@ package com.example.ratatouille23;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class Activity_Conferma_Codice extends AppCompatActivity {
+    TextView tvPromptColorato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conferma_codice);
+        tvPromptColorato = findViewById(R.id.textViewPromptColorato);
+        String testoPromptColorato ="<font color=#5F5959>Ora inserisci il </font><font color =#CE9B44>codice di verifica </font><font color=#5F5959>ricevuto e la </font><font color =#CE9B44>nuova password</font><font color=#5F5959>.</font>";
+        tvPromptColorato.setText(Html.fromHtml(testoPromptColorato));
+
     }
 }
