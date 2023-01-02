296c4a07b3221e9b01179d69f5b141e6bdb837b0
FerdiDale <fer.dalessandro@studenti.unina.it>
2023-01-02 19:40:54 +0100

    Interfaccia conferma codice per il recupero password quasi completata

--- CodiceClient/app/src/main/java/com/example/ratatouille23/Activity_Conferma_Codice.java
+++ CodiceClient/app/src/main/java/com/example/ratatouille23/Activity_Conferma_Codice.java
@@ -3,18 +3,57 @@ package com.example.ratatouille23;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity_Conferma_Codice extends AppCompatActivity {
-    TextView tvPromptColorato;
+    TextView textViewPromptColorato;
+    EditText editTextPassword;
+    EditText editTextConfermaPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conferma_codice);
-        tvPromptColorato = findViewById(R.id.textViewPromptColorato);
+        textViewPromptColorato = findViewById(R.id.textViewPromptColorato);
         String testoPromptColorato ="<font color=#5F5959>Ora inserisci il </font><font color =#CE9B44>codice di verifica </font><font color=#5F5959>ricevuto e la </font><font color =#CE9B44>nuova password</font><font color=#5F5959>.</font>";
-        tvPromptColorato.setText(Html.fromHtml(testoPromptColorato));
+        textViewPromptColorato.setText(Html.fromHtml(testoPromptColorato));
 
+    }
+
+    public void mostraNascondiPassword(View view) {
+
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfermaPassword = findViewById(R.id.editTextConfermaPassword);
+
+        if (view.getId()==R.id.iconaOcchioPassword) {
+            if (editTextPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+
+                //((ImageView(view)).setImageResource(R.drawable.hide_password);
+                editTextPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+            } else {
+
+                //((ImageView)(view)).setImageResource(R.drawable.show_password);
+                editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+            }
+        }
+        else {
+            if (editTextConfermaPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+
+                //((ImageView(view)).setImageResource(R.drawable.hide_password);
+                editTextConfermaPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+            } else {
+
+                //((ImageView)(view)).setImageResource(R.drawable.show_password);
+                editTextConfermaPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+            }
+        }
     }
 }
