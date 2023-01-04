05846c1c2a768b7bd9669cd334fa6b35c68b5d70
KamiDaga <95750384+KamiDaga@users.noreply.github.com>
2023-01-04 13:54:21 +0100

    Prova migrazione backend

---
+++ Database/ratatouille2023/src/test/java/com/fpsteam/ratatouille2023/Ratatouille2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpsteam.ratatouille2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ratatouille2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
