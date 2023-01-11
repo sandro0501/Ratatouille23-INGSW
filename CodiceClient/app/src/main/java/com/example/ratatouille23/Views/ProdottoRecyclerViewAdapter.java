16b7020482a04f20fac9f0c57d2eaf8bde435038
SantoloBarretta <san.barretta@studenti.unina.it>
2023-01-11 22:55:22 +0100

    completamento funzionalità grafiche per modifica prodotto. inizio implementazione funzionalità grafiche per eliminazione prodotto.

--- CodiceClient/app/src/main/java/com/example/ratatouille23/Views/ProdottoRecyclerViewAdapter.java
+++ CodiceClient/app/src/main/java/com/example/ratatouille23/Views/ProdottoRecyclerViewAdapter.java
@@ -1,14 +1,17 @@
 package com.example.ratatouille23.Views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ratatouille23.Controller;
 import com.example.ratatouille23.Models.Prodotto;
 import com.example.ratatouille23.R;
 
@@ -20,6 +23,7 @@ public class ProdottoRecyclerViewAdapter extends RecyclerView.Adapter<ProdottoRe
 
     private Context context;
     private ArrayList<Prodotto> dispensa;
+    private boolean isProdottoSottoSoglia;
 
     public ProdottoRecyclerViewAdapter(Context context, ArrayList<Prodotto> dispensa, RecyclerViewProdottoInterface recyclerViewInterfaceProdotto){
         this.context = context;
@@ -41,13 +45,48 @@ public class ProdottoRecyclerViewAdapter extends RecyclerView.Adapter<ProdottoRe
     public void onBindViewHolder(@NonNull ProdottoRecyclerViewAdapter.MyViewHolder holder, int position) {
         //assegna valori alle "card" che abbiamo creato in dipendenza anche della loro posizione nella lista recycler view
 
+        isProdottoSottoSoglia = Controller.getInstance().controllaSogliaProdotto(dispensa,position);
+
+        /*
+        if (Controller.getIsModalitaEliminazioneProdottoAttiva() == true){
+            Prodotto prodottoCorrente = dispensa.get(position);
+            CardView cardProdotto = holder.itemView.findViewById(R.id.cardViewProdotto);
+
+            if(prodottoCorrente.isSelected()){
+                cardProdotto.setCardBackgroundColor(Color.parseColor("#F2A726"));
+            } else {
+                cardProdotto.setCardBackgroundColor(Color.WHITE);
+            }
+
+            cardProdotto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    prodottoCorrente.setSelected(!prodottoCorrente.isSelected());
+                    if(prodottoCorrente.isSelected()){
+                        cardProdotto.setCardBackgroundColor(Color.parseColor("#F2A726"));
+                    } else {
+                        cardProdotto.setCardBackgroundColor(Color.WHITE);
+                    }
+
+                }
+            });
+
+        } */
+
         holder.nomeProdotto.setText(dispensa.get(position).getNome());
         holder.descrizioneProdotto.setText(dispensa.get(position).getDescrizione());
         holder.quantitaProdotto.setText(Double.toString(dispensa.get(position).getQuantita()));
+
+        if(isProdottoSottoSoglia){
+            holder.quantitaProdotto.setTextColor(Color.RED);
+        }
+
         holder.unitaProdotto.setText(dispensa.get(position).getUnita());
         holder.costoAcquistoProdotto.setText(dispensa.get(position).getCostoAcquisto());
 
     }
+
+
 
     @Override
     public int getItemCount() {
@@ -55,7 +94,7 @@ public class ProdottoRecyclerViewAdapter extends RecyclerView.Adapter<ProdottoRe
         return dispensa.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder{
         //una specie di metodo onCreate che prende gli elementi di cui sono formate le card dal file xml
         TextView nomeProdotto;
         TextView descrizioneProdotto;
@@ -72,15 +111,14 @@ public class ProdottoRecyclerViewAdapter extends RecyclerView.Adapter<ProdottoRe
             unitaProdotto = itemView.findViewById(R.id.textViewMisuraProdotto);
             costoAcquistoProdotto = itemView.findViewById(R.id.textViewCostoAcquitoProdotto);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+            itemView.findViewById(R.id.cardViewProdotto).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     if (recyclerViewInterfaceProdotto != null) {
                         int posizioneProdotto = getAdapterPosition();
                         if(posizioneProdotto!=RecyclerView.NO_POSITION){
-                            recyclerViewInterfaceProdotto.onProdottoClicked(posizioneProdotto);
+                                recyclerViewInterfaceProdotto.onProdottoClicked(posizioneProdotto, itemView);
                         }
-
                     }
                 }
             });
--- CodiceClient/app/src/main/java/com/example/ratatouille23/Views/RecyclerViewProdottoInterface.java
+++ CodiceClient/app/src/main/java/com/example/ratatouille23/Views/RecyclerViewProdottoInterface.java
@@ -1,5 +1,7 @@
 package com.example.ratatouille23.Views;
 
+import android.view.View;
+
 public interface RecyclerViewProdottoInterface {
-    void onProdottoClicked(int posizioneProdotto);
+    void onProdottoClicked(int posizioneProdotto, View itemView);
 }
