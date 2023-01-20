6fd5abbabfd917e32fefed70323cae259cd2a9c9
KamiDaga <kevin789@live.it>
2023-01-20 14:28:22 +0100

    Ripristino database

--- Database/ratatouille2023/src/main/java/com/fpsteam/ratatouille2023/repository/RepositoryUtente.java
+++
@@ -1,9 +0,0 @@
-package com.fpsteam.ratatouille2023.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fpsteam.ratatouille2023.entity.Utente;
-
-public interface RepositoryUtente extends JpaRepository<Utente,Integer>{
-
-}
