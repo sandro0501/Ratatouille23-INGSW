c09fc30598dbd60e21617102bcceff53cb38bddb
KamiDaga <kevin789@live.it>
2023-02-02 12:22:55 +0100

    Immagina avere tortoisegit come psicoterapeuta

---
+++ CodiceServer/ratatouille2023/src/main/java/com/fpsteam/ratatouille2023/handlers/HandleElemento.java
@@ -0,0 +1,10 @@
+package com.fpsteam.ratatouille2023.handlers;
+
+import com.fpsteam.ratatouille2023.entity.Elemento;
+import com.fpsteam.ratatouille2023.entity.SezioneMenu;
+
+public class HandleElemento 
+{
+	public Elemento elemento;
+	public SezioneMenu sezione;
+}
